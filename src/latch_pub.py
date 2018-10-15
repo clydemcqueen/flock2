aebdd437d87d0584a00940bddcf02b6d1956dd77
Clyde McQueen <clyde@mcqueen.net>
2018-10-15 11:59:54 -0700

    More precise latching test (not working)

--- src/latch_pub.py
+++ src/latch_pub.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
+import random
+
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 from std_msgs.msg import String
 
 
-class Latching(Node):
+class LatchPub(Node):
 
     def __init__(self):
-        super().__init__('latching')
+        super().__init__('latch_pub')
         latching_qos = QoSProfile(depth=1, durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
         pub = self.create_publisher(String, 'foo', qos_profile=latching_qos)
-        msg = String(data='test')
+        d = 'test%d' % random.randint(1, 101)
+        self.get_logger().info('Sending %s' % d)
+        msg = String(data=d)
         pub.publish(msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Latching()
+    node = LatchPub()
 
     try:
         rclpy.spin(node)
