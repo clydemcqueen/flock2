209b3609ca2ba7031c2484968d60535be8474999
Clyde McQueen <clyde@mcqueen.net>
2018-11-03 10:44:44 -0700

    Added util.duration

--- src/util.py
+++ src/util.py
@@ -1,17 +1,24 @@
 import time
 
 from builtin_interfaces.msg import Time
-from geometry_msgs.msg import Transform, TransformStamped, Vector3
+from geometry_msgs.msg import Pose, Transform, TransformStamped, Vector3
 
 
 def now():
+    """Return builtin_interfaces.msg.Time object with the current CPU time"""
     cpu_time = time.time()
     sec = int(cpu_time)
     nanosec = int((cpu_time - sec) * 1000000)
     return Time(sec=sec, nanosec=nanosec)
 
 
-def pose_to_transform(pose, stamp, parent_frame, child_frame):
+def duration(t1: Time, t2: Time):
+    """Compute duration from t1 to t2, return seconds (float)"""
+    return float(t2.sec - t1.sec) + float(t2.nanosec - t1.nanosec) / 1000000.
+
+
+def pose_to_transform(pose: Pose, stamp: Time, parent_frame: str, child_frame: str):
+    """Convert geometry_msgs.msg.Pose to a geometry_msgs.msg.Transform"""
     v = Vector3(x=pose.position.x, y=pose.position.y, z=pose.position.z)
     transform_stamped = TransformStamped()
     transform_stamped.header.stamp = stamp
