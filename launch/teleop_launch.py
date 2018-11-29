076d74be0ed03c8adb6503edf121c92115ef24b2
Clyde McQueen <clyde@mcqueen.net>
2018-11-28 17:56:39 -0800

    Open rviz2 with rviz file; make CLion happy

--- launch/teleop_launch.py
+++ launch/teleop_launch.py
@@ -3,6 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 # Full teleop launch: driver, base, joystick, etc.
 
@@ -10,6 +11,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     urdf = os.path.join(get_package_share_directory('flock2'), 'urdf', 'tello.urdf')
     return LaunchDescription([
+        ExecuteProcess(cmd=['rviz2', '-d', 'src/flock2/launch/default.rviz'], output='screen'),
         Node(package='robot_state_publisher', node_executable='robot_state_publisher', output='screen', arguments=[urdf]),
         Node(package='joy', node_executable='joy_node', output='screen'),
         Node(package='flock2', node_executable='flock_driver.py', output='screen'),
