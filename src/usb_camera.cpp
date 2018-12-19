0a85a958634fc1fc95b76b8b4cda75e07b387ef3
Clyde McQueen <clyde@mcqueen.net>
2018-12-19 12:51:50 -0800

    usb_camera publishes camera_info

--- src/usb_camera.cpp
+++ src/usb_camera.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "cv_bridge/cv_bridge.h"
 
@@ -9,20 +6,28 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/aruco.hpp"
 
+#include "get_camera_info.cpp"
+
 namespace usb_camera {
 
 class USBCamera : public rclcpp::Node
 {
 public:
 
-  explicit USBCamera() : Node("usb_camera"), stream1(0)
+  explicit USBCamera() : Node("usb_camera"), camera_(0)
   {
     camera_info_pub_ = create_publisher<sensor_msgs::msg::CameraInfo>("camera_info", 1);
     image_pub_ = create_publisher<sensor_msgs::msg::Image>("image_raw", 1);
 
-    if (!stream1.isOpened()) {
+    if (!camera_.isOpened()) {
       throw "cannot open camera";
     }
+
+    if (!get_camera_info(camera_info_msg_)) {
+      throw "cannot get camera info";
+    }
+
+    header_.frame_id = "camera_frame";
   }
 
   ~USBCamera() {}
@@ -30,31 +35,38 @@ public:
   void spin()
   {
     cv::Mat frame;
-    std_msgs::msg::Header header{};
-    header.frame_id = "camera_frame";
 
     while (rclcpp::ok()) {
       // Block until a frame is available
-      stream1.read(frame);
+      camera_.read(frame);
 
+      // Debugging
       cv::imshow("raw", frame);
       cv::waitKey(1);
 
-      // Publish image_raw if there are subscribers
+      // Synchronize messages
+      auto stamp = now();
+
+      // Publish image_raw
       if (count_subscribers(image_pub_->get_topic_name()) > 0) {
-        header.stamp = now();
-        cv_bridge::CvImage image{header, sensor_msgs::image_encodings::BGR8, frame};
+        header_.stamp = stamp;
+        cv_bridge::CvImage image{header_, sensor_msgs::image_encodings::BGR8, frame};
         image_pub_->publish(image.toImageMsg());
       }
 
       // Publish camera_info
-      // TODO
+      if (count_subscribers(camera_info_pub_->get_topic_name()) > 0) {
+        camera_info_msg_.header.stamp = stamp;
+        camera_info_pub_->publish(camera_info_msg_);
+      }
     }
   }
 
 private:
 
-  cv::VideoCapture stream1;
+  cv::VideoCapture camera_;
+  sensor_msgs::msg::CameraInfo camera_info_msg_;
+  std_msgs::msg::Header header_{};
 
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
   rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_pub_;
@@ -72,7 +84,6 @@ int main(int argc, char **argv)
 
   // Create node
   auto node = std::make_shared<usb_camera::USBCamera>();
-  auto result = rcutils_logging_set_logger_level(node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_INFO);
 
   // Spin until rclcpp::ok() returns false
   node->spin();
